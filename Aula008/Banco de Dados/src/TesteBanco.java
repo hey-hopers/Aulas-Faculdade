79039583ca98437a81bc277498d68c29d33b4507
Kauã Hoepers <88445747+hey-hopers@users.noreply.github.com>
2024-04-29 19:41:31 -0300

    Banco Aula

---
+++ Aula008/Banco de Dados/src/TesteBanco.java
@@ -0,0 +1,9 @@
+public class TesteBanco {
+    
+    public static void main(String[] args) {
+        
+        ConexaoBanco conexao = new ConexaoBanco();
+        conexao.getConexaoBanco();
+
+    }
+}
